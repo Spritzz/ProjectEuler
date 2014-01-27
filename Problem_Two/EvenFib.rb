f52cb8b62a1ebcbb46808b1a74802609d1589e6d
Spritzz <cz.bob.wang@gmail.com>
2014-01-26 22:39:04 -0800

    bob solution for P2

--- Problem_Two/EvenFib.rb
+++ Problem_Two/EvenFib.rb
@@ -1,7 +1,16 @@
-def fib(a, b)
- puts "hi"
+def fib_fake(a,b)
+  nextNum = a + b
+  puts nextNum
+  sum = 0 
+  if nextNum > 4000000
+    return 0
+  else
+    if nextNum % 2 == 0
+	  puts "even number is #{b}"
+	  sum = nextNum
+	end
+	sum = sum + fib_fake(b,nextNum)
+  end
 end
 
-sum = 0
-fib(0,1)
-
+puts "answer is  #{fib_fake(1,1)}"
